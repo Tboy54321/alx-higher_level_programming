944657424c91125ca94373d66a93c3d94720bb5b
tboy54321 <akinniranoluwatosin22@gmail.com>
2023-06-27 22:16:12 +0100

    empty class Square that defines a square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# 1-square.py written by Akinniran Oluwatosin
+
+
+class Square:
+    """A square"""
+
+
+    def __init__(self, size):
+        """Initializes the square object"""
+
+
+        self._Square__size = size
+        pass
