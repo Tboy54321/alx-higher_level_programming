02e5ab39ceb3b0f68ff4db309a6f9f6046a94160
tboy54321 <akinniranoluwatosin22@gmail.com>
2023-07-03 22:32:05 +0100

    Creating and defining the properties of a triangle

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""An empty class that does nothing"""
+"""An empty class that defines a rectangle"""
 
 
 class Rectangle:
-    """Just an empty class"""
+    """An empty class that represents a rectangle"""
     pass
