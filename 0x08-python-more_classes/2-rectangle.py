4cbab6dd43a43b1f0324ebd3a77c94b87e83ceee
tboy54321 <akinniranoluwatosin22@gmail.com>
2023-07-03 22:48:32 +0100

    Creating and defining the properties of a triangle

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -49,7 +49,9 @@ class Rectangle:
             self.__height = value
 
     def area(self):
+        """Return the area of the rectangle"""
         return (self.__width * self.__height)
 
     def perimeter(self):
+        """Returns the perimeter of the rectangle"""
         return ((2 * self.__width) + (2 * self.__height))
