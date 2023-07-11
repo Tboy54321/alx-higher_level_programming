ed5f3e133f69e2de618c2e4905062e5aa02ccb86
tboy54321 <akinniranoluwatosin22@gmail.com>
2023-07-11 20:16:02 +0100

    Python input and output

--- 0x0A-python-inheritance/3-to_json_string.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""function that returns json of any object"""
-
-
-import json
-
-def to_json_string(my_obj):
-    """creating the function"""
-    return json.dumps(my_obj)
--- 0x0A-python-inheritance/4-from_json_string.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""function that returns an object rep by a json string"""
-import json
-
-
-def from_json_string(my_str):
-    """converts the data from strings"""
-    return json.loads(my_str)
