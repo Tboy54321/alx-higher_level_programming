8d93092b65da93b66cdbb57a6559ecccb3332e77
tboy54321 <akinniranoluwatosin22@gmail.com>
2023-07-15 18:26:07 +0100

    rectangle class creation

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,97 @@
+#!/usr/bin/python3
+
+from models.base import Base
+
+
+class Rectangle(Base):
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value <= 0:
+            raise ValueError("width must be > 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        elif value <= 0:
+            raise ValueError("height must be > 0")
+        else:
+            self.__height = value
+
+    @property
+    def x(self):
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        elif value < 0:
+            raise ValueError("x must be >= 0")
+        else:
+            self.__x = value
+
+    @property
+    def y(self):
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        if type(value) is not int:
+            raise TypeError("y must be an integer")
+        elif value < 0:
+            raise ValueError("y must be >= 0")
+        else:
+            self.__y = value
+
+    def area(self):
+        return self.width * self.height
+
+    def display(self):
+        for _ in range(self.y):
+            print()
+        for _ in range(self.height):
+            print(" " * self.x + self.width * "#")
+
+    def __str__(self):
+        return (f"[Rectangle] ({self.id}) {self.x}/{self.y} - {self.width}/{self.height}")
+
+
+    def update(self, *args, **kwargs):
+        list_ = ["id", "width", "height", "x", "y"]
+        x = 0
+        if args is not None:
+            for _ in list_:
+                try:
+                    """args[x] = list_[x]"""
+                    setattr(self, _, args[x])
+
+                except IndexError:
+                    pass
+                x += 1
+        else:
+            for _ in list_:
+                try:
+                    setattr(self, _, kwargs[_])
+                except KeyError:
+                    pass
---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+from models.rectangle import Rectangle
+
+class Square(Rectangle):
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+    
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
+
+    def update(self, *args):
+        list_ = ["id", "size", "x", "y"]
+
+        
