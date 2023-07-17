0bc75befc0b6f8905a158aed4d58e3e8456b8016
Tboy54321 <akinniranoluwatosin22@gmail.com>
2023-07-17 23:15:25 +0100

    updated square.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -20,7 +20,7 @@ class Base:
     def to_json_string(list_dictionaries):
         """To json string"""
         if list_dictionaries is None:
-            return []
+            return "[]"
         else:
             return json.dumps(list_dictionaries)
 
