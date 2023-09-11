80f94b243efd58df6bb6b455afdbbcc5f5ac85a5
tboy54321 <akinniranoluwatosin22@gmail.com>
2023-09-11 23:58:50 +0100

    Js warm up

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+argc = process.argv;
+
+if (argc[2] === '' && argc[3] === '') {
+	argc[3] = 'undefined';
+	argc[2] = 'undefined';
+	console.log(`${argc[2]} is ${argc[3]}`);
+}
+if (argc[3] === "") {
+	argc[3] = 'undefined';
+	console.log(`${argc[2]} is ${argc[3]}`);
+} else
+{
+	console.log(`${argc[2]} is ${argc[3]}`);
+}
