0af2db3e2909316634986ccfde3518634adb5f29
Tboy54321 <akinniranoluwatosin22@gmail.com>
2023-09-11 15:27:24 -0700

    JS WARM UP

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,9 +2,10 @@
 
 argc = process.argv.length - 2;
 
-if (argc === 0)
-	console.log('No argument');
-else if (argc === 1)
-	console.log('Argument found');
-else
-	console.log('Arguments found');
+if (argc === 0) {
+  console.log('No argument');
+} else if (argc === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const argument = process.argv[2];
+
+if (argument)
+{
+	console.log(argument);
+}
+else
+{
+	console.log("No argument");
+}
