2deb158abc237113be3522f738c9ef680f785434
Tboy54321 <akinniranoluwatosin22@gmail.com>
2023-09-11 15:03:29 -0700

    JS WARM UP

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,12 +2,9 @@
 
 numberOfArguments = process.argv.length - 2;
 
-if (numberOfArguments === 0) {
+if (numberOfArguments === 0)
 	console.log('No argument');
-}
-else if (numberOfArguments === 1) {
+else if (numberOfArguments === 1)
 	console.log('Argument found');
-}
-else {
+else
 	console.log('Arguments found');
-}
