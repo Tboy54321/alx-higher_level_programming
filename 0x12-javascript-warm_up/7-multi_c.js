f6a9e8d95b46c1285ff9f8afb68f53e68807e525
tboy54321 <akinniranoluwatosin22@gmail.com>
2023-09-12 01:14:28 +0100

    JS WARMUP

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const argc = process.argv[2]
+
+if (!isNaN(argc)) {
+	for (i = 0; i < argc; i++) {
+		console.log('C is fun')
+	}
+} else {
+	console.log('Missing number of occurrences')
+}
