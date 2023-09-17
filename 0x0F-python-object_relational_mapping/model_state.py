1811dba710b808d13264f8b12e949e014079d7c7
Tboy54321 <akinniranoluwatosin22@gmail.com>
2023-09-17 16:27:05 -0700

    FIRST MODEL

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-'''
+"""
 Creating my first model
-'''
+"""
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -9,10 +9,12 @@ Base = declarative_base()
 
 
 class State(Base):
+    """
+    class state inheriting from base class
+    """
+
     __tablename__ = 'states'
 
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
 
-    def __repr__(self):
-        return f"<State(id={self.id}, name='{self.name}')>"
