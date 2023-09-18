6ef457a90973c09a3ad20b647d13d1530e910b40
Tboy54321 <akinniranoluwatosin22@gmail.com>
2023-09-17 18:31:48 -0700

    filter states

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,40 +1,40 @@
 #!/usr/bin/python3
-'''
-Write a script that lists all states from the database
-'''
-import MySQLdb
-import sys
+"""
+This script  takes in the name of a state
+as an argument and lists all cities of that
+state, using the database `hbtn_0e_4_usa`.
+"""
 
-
-def list_states(username, password, database_name, state_name):
-
-    db = MySQLdb.connect(
-            host="localhost",
-            port=3306,
-            user=username,
-            passwd=password,
-            db=database_name
-            )
-
-    cursor = db.cursor()
-
-    cursor.execute("SELECT cities.id, cities.name FROM cities JOIN states ON cities.state_id = states.id WHERE states.name LIKE BINARY %(state_name)s ORDER BY cities.id", (state_name,))
-
-    rows = cursor.fetchall()
-
-    for row in rows:
-        print(row)
-
-    cursor.close()
-    db.close()
-
+import MySQLdb as db
+from sys import argv
 
 if __name__ == "__main__":
-    if len(sys.argv) != 5:
-        print("Usage: python script.py <username> <password> <database_name>")
-    else:
-        username = sys.argv[1]
-        password = sys.argv[2]
-        database_name = sys.argv[3]
-        state_name = sys.argv[4]
-        list_states(username, password, database_name, state_name)
+    """
+    Access to the database and get the cities
+    from the database.
+    """
+
+    db_connect = db.connect(host="localhost", port=3306,
+            user=argv[1], passwd=argv[2], db=argv[3])
+
+    with db_connect.cursor() as db_cursor:
+        db_cursor.execute("""
+            SELECT
+                cities.id, cities.name
+            FROM
+                cities
+            JOIN
+                states
+            ON
+                cities.state_id = states.id
+            WHERE
+                states.name LIKE BINARY %(state_name)s
+            ORDER BY
+                cities.id ASC
+        """, {
+            'state_name': argv[4]
+            })
+        rows_selected = db_cursor.fetchall()
+
+    if rows_selected is not None:
+        print(", ".join([row[1] for row in rows_selected]))
