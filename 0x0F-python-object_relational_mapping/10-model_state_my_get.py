f0a65fc8153fddd78f879ab5e307d825a7f7ab85
Tboy54321 <akinniranoluwatosin22@gmail.com>
2023-09-17 17:58:38 -0700

    fetch all states

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -18,11 +18,19 @@ def get_state(username, password, database_name, search_name):
 
     session = Session()
 
+    state = session.query(State).filter(State.name == search_name).first()
+    if state is not None:
+        print('{0}'.format(state.id))
+    else:
+        print("Not found")
+
+    '''
     try:
         state = session.query(State).filter(State.name == search_name).first()
         print(state.id)
     except NoResultFound:
         print("Not found")
+    '''
 
     session.close()
 
