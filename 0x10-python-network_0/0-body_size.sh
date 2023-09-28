31427d893a72c17b153a460cf719cf832db5c0a4
tboy54321 <akinniranoluwatosin22@gmail.com>
2023-09-28 22:02:12 +0100

    curl body size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,17 +1,8 @@
 #!/bin/bash
 
 # Check if a URL is provided as an argument
-if [ $# -eq 0 ]; then
-	echo "Usage: $0 <URL>"
-	exit 1
-fi
 
-URL=$1
+RESPONSE=$(curl -sI "$1" | grep -i content-length | awk '{print $2}' | tr -d '\r')
 
-RESPONSE=$(curl -sI $URL | grep -i content-length | awk '{print $2}' | tr -d '\r')
+echo "$RESPONSE"
 
-if [ -z "$RESPONSE" ]; then
-	echo "Error: Unable to retrieve response size."
-else
-	echo "$RESPONSE"
-fi
