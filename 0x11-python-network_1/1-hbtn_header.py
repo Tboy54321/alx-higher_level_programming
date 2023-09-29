9e0e4261b67520f278bffd98f731b1cfaa362211
tboy54321 <akinniranoluwatosin22@gmail.com>
2023-09-29 13:44:57 -0700

    Python script that fetches header from a site

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Python script that takes in a URL, sends a request to the URL"""
+
+
+import urllib.request
+from sys import argv
+
+
+def fetch_header(url):
+    """fetch_header class"""
+    with urllib.request.urlopen(url) as request:
+        response = request.info()
+        print(response['X-Request-Id'])
+
+if __name__ == "__main__":
+    fetch_header(argv[1])
